09d5d03e24cf60eaa542d28d92ecdd213b955f27
truong_napan <truongnapan111@gmail.com>
2024-10-10 14:22:24 +0700

    123

--- src/components/LandingPage/components/Header.tsx
+++ src/components/LandingPage/components/Header.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { Button, Layout, Typography } from 'antd';
 import Link from 'next/link';
-import React from 'react';
+import Image from 'next/image';
 
+import React from 'react';
+import Img from '/src/assets/favicon.ico'
 const { Header } = Layout;
 
-export default function LandingHeader()  {
+export default function LandingHeader() {
     return (
-        <Header className='bg-slate-50 block items-center justify-between p-0 mx-[10px] lg:mx-[150px] md:mx-[100px] sm:flex sm:h-[92px]'>
+        <Header className='bg-slate-50 block items-center justify-between 
+        p-0 mx-[10px] mb-8 mt-3 sm:mx-0 sm:mb-0 sm:mt-0 lg:mx-[150px] md:mx-[100px] sm:flex sm:h-[92px]'>
             <div className='flex items-center '>
                 <div className="logo">
                     <Link href={'/'}>
-                        <Typography className='font-bold text-4xl items-center text-cyan-500' >G</Typography>
+                        <Image src={Img} alt='' className='items-center w-10' />
                     </Link>
                 </div>
                 <div className='flex'>
@@ -29,7 +32,8 @@ export default function LandingHeader()  {
 
             <div className=''>
                 <Link href="/sign-up">
-                    <Button style={{ boxShadow: 'none' }} className="p-5 rounded-full text-sm font-medium border-1 text-cyan-600 border-cyan-600 hover:text-cyan-400 hover:border-cyan-400 hover:bg-slate-100 transition-all duration-200">
+                    <Button style={{ boxShadow: 'none' }} className="p-5 rounded-full text-sm font-medium border-1 
+                    text-cyan-600 border-cyan-600 hover:text-cyan-400 hover:border-cyan-400 hover:bg-slate-100 transition-all duration-200">
                         Đăng ký
                     </Button>
                 </Link>
