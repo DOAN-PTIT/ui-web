cb619c35924020562ec50aadb059cb74b89ddea9
truong_napan <truongnapan111@gmail.com>
2024-12-05 21:59:27 +0700

    fix

--- src/styles/layoutStyle.tsx
+++ src/styles/layoutStyle.tsx
@@ -1,5 +1,5 @@
 
 import tw from "tailwind-styled-components";
 export const LayoutStyled = tw.div`
-  min-h-screen rounded-tl-lg overflow-auto overflow-x-hidden p-5 gap-5
+  min-h-screen rounded-t-lg overflow-auto overflow-x-hidden p-5 gap-5
 `
